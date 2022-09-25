3b9fba9ae93311f0a583c0ee60a57790992f2574
greysoh <greysoh@pm.me>
2022-09-25 15:30:48 -0700

    feature: Implements new API.

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ module.exports = class LibIncenerator {
 
     this.config = config;
 
-    this.broadcasts = [];
     this.eventListeners = {
       "connection": []
     }
@@ -24,76 +23,14 @@ module.exports = class LibIncenerator {
     if (!this.config.disableAutostart) this.init();
   }
 
-  connectionClass(socket, broadcasts) {
-    return class Socket {
-      constructor(uuid) {
-        this.uuid = uuid;
-        this.eventListeners = {
-          "message": []
-        };
-
-        this.internalRecv();
-      }
-
-      send(data) {
-        const bufferData = Buffer.isBuffer(data) ? data : Buffer.from(data);
-        const hexString = bufferData.toString("hex");
-
-        socket.send(JSON.stringify({
-          "type": "data_response",
-          "uuid": this.uuid,
-          "data": hexString
-        }));
-      }
-
-      async internalRecv() {
-        function sleep(ms) {
-          return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
-        while (true) {
-          for (const i of broadcasts) {
-            if (!i) continue;
-            
-            if (i.type == "data" && i.UUID == this.uuid) {
-              const localCopy = JSON.parse(JSON.stringify(i));
-
-              delete broadcasts[broadcasts.indexOf(i)];
-
-              try {
-                this.eventListeners["message"].forEach(i => i(Buffer.from(localCopy.data, "hex")));
-              } catch (e) {
-                console.error(e);
-              }
-            }
-          }
-
-          await sleep(10);
-        }
-      }
-
-      on(event, callback) {
-        if (typeof callback != "function") {
-          throw new Error("Callback is not a function");
-        }
-
-        if (!this.eventListeners[event]) {
-          throw new Error("Event does not exist");
-        }
-
-        this.eventListeners[event].push(callback);
-      }
-    }
-  }
-
-  init() {
+  async init() {
     // Connect to server
     this.socket = new WebSocket(this.config.host);
 
-    this.socket.on("open", () => {
+    this.socket.on("open", async() => {
       this.socket.send("Accept: Bearer " + this.config.password);
 
-      this.socket.on("message", (data) => {
+      this.socket.on("message", async(data) => {
         const strData = data.toString();
 
         if (strData.startsWith("AcceptResponse Bearer")) {
@@ -106,11 +43,15 @@ module.exports = class LibIncenerator {
           const json = JSON.parse(strData);
 
           if (json.type == "connection") {
-            const connectionClass = this.connectionClass(this.socket, this.broadcasts);
-            const socket = new connectionClass(json.uuid);
-            this.eventListeners["connection"].forEach(i => i(socket));
-          } else if (json.type == "data") {
-            this.broadcasts.push(json);
+            const ws = new WebSocket(`${this.config.host}/trident/${json.id}/${this.config.password}`);
+
+            while (true) {
+              if (ws.readyState == WebSocket.OPEN) {
+                return this.eventListeners["connection"].forEach(i => i(ws));
+              }
+
+              await new Promise(i => setTimeout(i, 10));
+            }
           }
         }
       });
